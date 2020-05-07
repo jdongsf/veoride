ef8ae4a15afaf0fdf55c040bcbb031a4683849c6
jundong <jdongsf@gmail.com>
2020-05-06 17:57:47 -0700

    Polish code

--- app/src/main/java/com/interview/evoride/MapViewModel.kt
+++ app/src/main/java/com/interview/evoride/MapViewModel.kt
@@ -4,25 +4,26 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.*
 import com.google.maps.android.SphericalUtil
 
 @Suppress("DEPRECATION")
 class MapViewModel : ViewModel() {
 
     private var startTime: Long = 0
+    private var minLat: Double? = null
+    private var maxLat: Double? = null
+    private var minLng: Double? = null
+    private var maxLng: Double? = null
     private var totalDistance: Double = 0.0
 
     private var dst: LatLng? = null
-    private var startMarker: Marker? = null
     private var endMarker: Marker? = null
+    private var startMarker: Marker? = null
     private val route = mutableListOf<LatLng>()
 
     internal lateinit var map: GoogleMap
-    internal val arrived = MutableLiveData<Boolean>()
+    internal val arrived = MutableLiveData<LatLngBounds>()
 
     fun initMap(googleMap: GoogleMap) {
         map = googleMap
@@ -33,11 +34,11 @@ class MapViewModel : ViewModel() {
         }
     }
 
-    fun init() {
+    private fun init() {
         map.isMyLocationEnabled = true
         map.setOnMapClickListener(object : GoogleMap.OnMapClickListener {
             override fun onMapClick(p0: LatLng?) {
-                if (startTime != 0L) return
+                if (isStarted()) return
                 dst = p0
                 endMarker?.remove()
                 endMarker = map.addMarker(MarkerOptions().position(dst!!).title("End"))
@@ -45,38 +46,48 @@ class MapViewModel : ViewModel() {
         })
         map.setOnMyLocationChangeListener { arg ->
             val loc = LatLng(arg.latitude, arg.longitude)
-            if (route.isNotEmpty() && startTime != 0L) {
+            if (isStarted() && route.isNotEmpty()) {
                 val pre = route[route.size - 1]
                 if (pre == loc) return@setOnMyLocationChangeListener
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, ZOOM))
                 map.addPolyline(PolylineOptions().clickable(true).add(pre, loc))
                 totalDistance += SphericalUtil.computeDistanceBetween(pre, loc)
                 // reach the destination
-                if (dst != null)
-                    println("distance is :" + SphericalUtil.computeDistanceBetween(loc, dst))
-                if (dst != null && SphericalUtil.computeDistanceBetween(
-                        loc,
-                        dst
-                    ) < RADIUS && arrived.value != true
-                ) {
-                    arrived.value = true
+//                if (dst != null)
+//                    println("distance is :" + SphericalUtil.computeDistanceBetween(loc, dst))
+                if (isArrived(loc)) {
+                    arrived.value = LatLngBounds(LatLng(minLat!!, minLng!!), LatLng(maxLat!!, maxLng!!))
                 }
             } else {
                 if (startMarker == null)
                     map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, ZOOM))
                 else
-                    startMarker!!.remove()
+                    startMarker?.remove()
                 startMarker = map.addMarker(MarkerOptions().position(loc).title("Start"))
             }
 
-            if (startTime == 0L && route.isNotEmpty()) route[0] = loc
+            if (route.isNotEmpty() && !isStarted()) route[0] = loc
             else route.add(loc)
+
+            minLat = if(minLat == null) loc.latitude else  Math.min(minLat!!, loc.latitude)
+            maxLat = if (maxLat == null) loc.latitude else  Math.max(maxLat!!, loc.latitude)
+            minLng = if (minLng == null) loc.longitude else  Math.max(minLng!!, loc.longitude)
+            maxLng = if (maxLng == null) loc.longitude else  Math.max(maxLng!!, loc.longitude)
         }
+    }
+
+    private fun isArrived(loc: LatLng): Boolean {
+        return dst != null &&
+                SphericalUtil.computeDistanceBetween(loc, dst) < RADIUS &&
+                arrived.value == null
+    }
+
+    private fun isStarted(): Boolean {
+        return startTime != 0L
     }
 
     fun travel() {
         startTime = System.currentTimeMillis()
-        if (!map.isMyLocationEnabled)
-            init()
     }
 
     fun totalDistance(): String {
@@ -89,6 +100,10 @@ class MapViewModel : ViewModel() {
         val m: Long = seconds / 60 % 60
         val h: Long = seconds / (60 * 60) % 24
         return String.format("Total time is %d:%02d:%02d", h, m, s)
+    }
+
+    fun isDstChoosed(): Boolean {
+        return dst != null
     }
 
     companion object {
--- app/src/main/java/com/interview/evoride/MapsActivity.kt
+++ app/src/main/java/com/interview/evoride/MapsActivity.kt
@@ -1,10 +1,12 @@
 package com.interview.evoride
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -27,32 +29,53 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         start.setOnClickListener {
-            it.visibility = GONE
-            viewModel.travel()
-        }
-        viewModel.arrived.observe(this, Observer {
-            if (it) {
-                viewModel.map.snapshot { bitmap -> thumbnail.setImageBitmap(bitmap) }
-                timeSummary.text = viewModel.totalTime()
-                distanceSummary.text = viewModel.totalDistance()
-
-                mapFragment.view?.visibility = GONE
-                summaryContainer.visibility = VISIBLE
+            if (viewModel.isDstChoosed()) {
+                it.visibility = GONE
+                viewModel.travel()
+            } else {
+                Toast
+                    .makeText(this, "Choose destination at first", Toast.LENGTH_SHORT)
+                    .show()
             }
+        }
+
+        viewModel.arrived.observe(this, Observer {
+            viewModel.map.setLatLngBoundsForCameraTarget(it)
+            viewModel.map.snapshot { bitmap -> thumbnail.setImageBitmap(bitmap) }
+            timeSummary.text = viewModel.totalTime()
+            distanceSummary.text = viewModel.totalDistance()
+
+            mapFragment.view?.visibility = GONE
+            summaryContainer.visibility = VISIBLE
         })
 
         requestPermissionIfNeeded()
     }
 
     private fun requestPermissionIfNeeded() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                0
+            )
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED))
-            viewModel.init()
+    // restart activity once the permission is granted
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+            startActivity(Intent(this, MapsActivity::class.java))
+            finish()
+        }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
